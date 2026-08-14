a28a7fb9c5aac22eb431d13b88a48ee7ca842519
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-14 20:34:57 +0000

    V0.38.3.7

--- web/server/main.js
+++ web/server/main.js
@@ -33,7 +33,7 @@ api.post("/api/login", async (req, res) => {
         }
 
         if (password === row.password) {
-            res.status(200).json({ "token": row.token })
+            res.status(200).json({ "token": row.token, "name": data["server-name"] })
         } else {
             return res.status(401).json({ "error": "user_pass" })
         }
