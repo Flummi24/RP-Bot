49474d2333db8b3124afbbfb2140ca2ca423d688
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-14 18:22:32 +0000

    V0.38.3.7

---
+++ features/anti-nuke/main.js
@@ -0,0 +1,50 @@
+const data = require("../../data/data.json")
+
+module.exports = (client) => {
+    client.on('guildMemberAdd', async (member) => {
+    if (!member.user.bot) return;
+
+    await member.ban({
+        reason: 'Anti Nuke'
+    });
+    
+    if (data.logging.enabled === true && data.logging["anti-nuke"]) {
+        if (data.discord["use-container"]) {
+            return;
+        } else {
+            
+        }
+    }
+
+    const logs = await member.guild.fetchAuditLogs({
+        type: 28,
+        limit: 1
+    });
+
+    const entry = logs.entries.first();
+   
+
+    if (!entry) return;
+
+    const inviter = entry.executor;
+
+    console.log(
+        `${inviter.tag} hat den Bot ${member.user.tag} eingeladen`
+    );
+
+
+try {
+
+    // Optional: Einlader bannen
+    const inviterMember = await member.guild.members.fetch(inviter.id);
+
+    await inviterMember.ban({
+        reason: 'Sicherheitsban'
+    });
+       console.log(`ANTI NUKE: Der User ${inviter.tag} wurde Gebannt`)
+
+} catch (error) {
+console.log(error.message)
+}
+});
+};
---
+++ utils/builders.js
@@ -0,0 +1,3 @@
+async function container(title, content) {
+    
+}
