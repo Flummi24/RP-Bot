850f29c544d512c46cd10737228878b9692da570
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-15 16:33:23 +0000

    V0.38.3.7

--- features/personal/main.js
+++ features/personal/main.js
@@ -3,3 +3,15 @@ const db = require("../../utils/database.js")
 async function generate(username) {
     
 }
+
+async function create(username, name, geburt) {
+    
+}
+
+async function remove(username) {
+    
+}
+
+async function edit(username, key, value) {
+    
+}
