11c408b9f27155c34a3addb0b51afa95ef72721d
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-15 12:01:28 +0000

    V0.38.3.7

--- main.js
+++ main.js
@@ -8,5 +8,8 @@ const client = require("./init/client.js")
 const anti_nuke = require("./features/anti-nuke/main.js")
 anti_nuke(client)
 
+const teamwarns = require("./features/teamwarns/main.js")
+teamwarns(client)
+
 const TOKEN = process.env.TOKEN;
 client.login(TOKEN)
