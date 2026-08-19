8de442dd0412435c1708d04426f95b2f2d87e94f
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-19 16:29:53 +0000

    V0.38.3.7

--- utils/output.js
+++ utils/output.js
@@ -1,7 +1,7 @@
 const { ebuilder, cbuilder } = require("./builders.js")
 const data = require("../data/data.json")
 
-async function output(title, content, color) {
+function output(title, content, color) {
     if (data.discord["use-container"]) {
         return { flags: 1 << 15, components: cbuilder(title, content) }
     } else {
