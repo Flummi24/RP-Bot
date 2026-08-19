f2cd286d4bae7c32c827eb7847f01bb8be7747f5
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-19 16:44:25 +0000

    V0.38.3.7

--- utils/permissions.js
+++ utils/permissions.js
@@ -2,7 +2,7 @@ const data = require("../data/data.json")
 
 async function check(member) {
     if (!member.roles.cache.has(data["admin-role"])) {
-            return { content: `Dir fehlen die folgenden Rechte: <@${data["admin-role"]}>`, ephemeral: true };
+            return { content: `Dir fehlen die folgenden Rechte: <@&${data["admin-role"]}>`, ephemeral: true };
     } else {
         return false
     }
