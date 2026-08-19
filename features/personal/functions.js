e3e9f8f2accbd030c03c599292cf0217581fe703
Flummi24 <183976293+Flummi24@users.noreply.github.com>
2026-08-19 18:18:01 +0000

    V0.38.3.7

--- features/personal/functions.js
+++ features/personal/functions.js
@@ -146,9 +146,9 @@ if (waffe_groß) waffenscheine.push("Groß");
 
     bild.fillText(`Name: ${name}`, 22, 125);
     bild.fillText(`Geburtsdatum: ${geburt}`, 22, 160);
-    bild.fillText(`Führerscheine: ${scheine.join(", ") || "Keine"}`, 22, 235);
-    bild.fillText(`Großer Waffenschein: ${waffe_groß ? "Ja" : "Nein"}`, 22, 270);
-    bild.fillText(`Kleiner Waffenschein: ${waffe_klein ? "Ja" : "Nein"}`, 22, 305);
+    bild.fillText(`Führerscheine: ${scheine.join(", ") || "Keine"}`, 22, 220);
+    bild.fillText(`Großer Waffenschein: ${waffe_groß ? "Ja" : "Nein"}`, 22, 255);
+    bild.fillText(`Kleiner Waffenschein: ${waffe_klein ? "Ja" : "Nein"}`, 22, 290);
 
     const buffer = canvas.toBuffer('image/png');
 
